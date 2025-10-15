3ae52595c4ba9af4d0bc4ad41049a637f2eeef4a
tahawael <62266433+tahawael@users.noreply.github.com>
2025-10-15 17:42:36 +0300

    Delete gifs/tiles_3/lmasd.sh

--- gifs/tiles_3/lmasd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Navigate to your folder
-cd "C:/VisualStudio codes/discord.go/src/gifs/tiles_3" || exit
-
-# Resize all PNG files to 128x128
-for file in *.png; do
-    magick convert "$file" -resize 128x128 "128_${file}"
-done
